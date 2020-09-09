9202d38cd96075eacdab717f3e08273cb7171a3d
WitcherTraining <witcher236@gmail.com>
2020-09-09 22:08:17 +0600

    adding POJOs, closes #2 issue

---
+++ src/main/java/kz/springcourse/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package kz.springcourse.petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/kz/springcourse/petclinic/model/Vet.java
@@ -0,0 +1,6 @@
+package kz.springcourse.petclinic.model;
+
+public class Vet extends Person {
+
+
+}
