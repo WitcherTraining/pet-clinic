708122afdcaab59967c5bc4e68997acf677d279d
WitcherTraining <witcher236@gmail.com>
2020-09-20 21:23:57 +0600

    Added Service to Specialties, closes #31

---
+++ pet-clinic-data/src/main/java/kz/springcourse/petclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package kz.springcourse.petclinic.services;
+
+import kz.springcourse.petclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
--- pet-clinic-web/src/main/java/kz/springcourse/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/kz/springcourse/petclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package kz.springcourse.petclinic.bootstrap;
 
 import kz.springcourse.petclinic.model.Owner;
+import kz.springcourse.petclinic.model.Pet;
 import kz.springcourse.petclinic.model.PetType;
 import kz.springcourse.petclinic.model.Vet;
 import kz.springcourse.petclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import kz.springcourse.petclinic.services.PetTypeService;
 import kz.springcourse.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -36,18 +39,48 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Zhenya");
         owner1.setLastName("Protas");
+        owner1.setAddress("Krylova 58");
+        owner1.setCity("Karaganda");
+        owner1.setTelephone("87002222222");
+
+        Pet zhenyasPet = new Pet();
+        zhenyasPet.setName("Lisa");
+        zhenyasPet.setPetType(savedCatPetType);
+        zhenyasPet.setBirthDate(LocalDate.now());
+        zhenyasPet.setOwner(owner1);
+        owner1.getPets().add(zhenyasPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Lera");
         owner2.setLastName("Akimova");
+        owner2.setAddress("Krivoguza 44");
+        owner2.setCity("Karaganda");
+        owner2.setTelephone("870034234234");
+
+        Pet lerasPet = new Pet();
+        lerasPet.setName("Samsa");
+        lerasPet.setPetType(savedCatPetType);
+        lerasPet.setBirthDate(LocalDate.now());
+        lerasPet.setOwner(owner2);
+        owner2.getPets().add(lerasPet);
 
         ownerService.save(owner2);
 
         Owner owner3 = new Owner();
         owner3.setFirstName("Lev");
         owner3.setLastName("Rasputin");
+        owner3.setAddress("Prospect Mira 8");
+        owner3.setCity("Temirtau");
+        owner3.setTelephone("870532594551");
+
+        Pet levsPet = new Pet();
+        levsPet.setName("Chernysh");
+        levsPet.setPetType(savedDogPetType);
+        levsPet.setBirthDate(LocalDate.now());
+        levsPet.setOwner(owner3);
+        owner3.getPets().add(levsPet);
 
         ownerService.save(owner3);
 
