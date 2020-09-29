ac86c716d97cd55924019e381435971eb77c5d9d
WitcherTraining <witcher236@gmail.com>
2020-09-29 21:32:06 +0600

    Updated Visit to JPA entity, closes #38

--- pet-clinic-data/src/main/java/kz/springcourse/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/kz/springcourse/petclinic/model/Pet.java
@@ -2,6 +2,8 @@ package kz.springcourse.petclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -20,6 +22,9 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Vet> vets = new HashSet<>();
 
     public String getName() {
         return name;
@@ -51,5 +56,13 @@ public class Pet extends BaseEntity {
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Vet> getVets() {
+        return vets;
+    }
+
+    public void setVets(Set<Vet> vets) {
+        this.vets = vets;
     }
 }
