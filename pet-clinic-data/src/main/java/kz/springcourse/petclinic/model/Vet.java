0a21f5aba1e0718b636b78578b31633bf61e1702
WitcherTraining <witcher236@gmail.com>
2020-10-05 18:19:35 +0600

    Added Lombok project, closes #51

--- pet-clinic-data/src/main/java/kz/springcourse/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/kz/springcourse/petclinic/model/Vet.java
@@ -1,24 +1,23 @@
 package kz.springcourse.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
 
     @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(name = "vet_specialties",
-            joinColumns = @JoinColumn(name = "vet_id"),
-            inverseJoinColumns = @JoinColumn(name = "specialty_id"))
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Specialty> specialties = new HashSet<>();
 
-    public Set<Specialty> getSpecialties() {
-        return specialties;
-    }
-
-    public void setSpecialties(Set<Specialty> specialties) {
-        this.specialties = specialties;
-    }
 }
