b970535a3bd096964eba0d7b5e429d920b88ee3a
WitcherTraining <witcher236@gmail.com>
2020-10-24 22:51:41 +0600

    Implemented find Owner feature, closes #54

--- pet-clinic-data/src/main/java/kz/springcourse/petclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/kz/springcourse/petclinic/repositories/OwnerRepository.java
@@ -3,7 +3,11 @@ package kz.springcourse.petclinic.repositories;
 import kz.springcourse.petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/kz/springcourse/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/kz/springcourse/petclinic/services/OwnerService.java
@@ -2,8 +2,12 @@ package kz.springcourse.petclinic.services;
 
 import kz.springcourse.petclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 
 }
